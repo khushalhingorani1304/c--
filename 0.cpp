9c95b3efb80c1f649bae4f07e90049c83393b619
khushalhingorani1304 <khushalhingorani1304@gmail.com>
2024-02-24 21:44:48 +0530

    1

--- 0.cpp
+++ 0.cpp
@@ -1,17 +1,17 @@
 // //PROBLEM STATMENT -->>  A company wants to keep track of its employees.the company as three types of employees:Full-time ,part time and consultant.each employee has a name, id number, and salary. the full-time employees work 40 hours a week,part time employees work 20 hours a week,and Consultant are paid based on the hours they work. the company wants a program that can store and manipulate information about its employees.//
 
 
-// #include <iostream>
-// #include <string>
-// using namespace std;
-// class employee_3802{ 
-//     protected:
-//     string Name;
-//     int Id_num;
-//     double Salary;
-//     int Hours;
+#include <iostream>
+#include <string>
+using namespace std;
+class employee_3802{ 
+    protected:
+    string Name;
+    int Id_num;
+    double Salary;
+    int Hours;
 
-// };
+};
 
 
 // class full_time{
@@ -47,15 +47,15 @@
 // }
 
 
-#include <iostream>
-#include <iomanip>
-using namespace std;
+// #include <iostream>
+// #include <iomanip>
+// using namespace std;
 
-int main()
-{
-    int sum = 345;
-    setfill('+');
-    setprecision();
-    cout<<setw(20)<<sum;
-    return 0;
-}
+// int main()
+// {
+//     int sum = 345;
+//     setfill('+');
+//     setprecision();
+//     cout<<setw(20)<<sum;
+//     return 0;
+// }
